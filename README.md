bd82680f5a9d6e71fe29b6d236928c57ab75c73f
chaitanyathengdi <thengdi.chaitanya.sudhir@gmail.com>
2017-01-23 16:24:00 +0530

    Add CRUD operations

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 This repo simply holds a list that I keep adding to/removing from articles that I want to read; and posted as to be accessible and editable from anywhere.
 
 After months of sending myself emails on Gmail, I think this is better :)
+
+2017-01-23:
+Added crud operations.
+
+The app is still technically using a single template and uses ng-hide/ng-show for hiding/showing pages. 
+Could add ui-router to this at some point for the sake of doing things right, but feel this app seems a bit small for that.
+
+Also, if I make any changes to the list, I need to manually save the list by copy-paste. I'd need a backend for getting that to work via the UI, 
+but the task seems a bit trivial for needing to add a whole backend to this.
+
+
--- app.js
+++ app.js
@@ -11,12 +11,47 @@ function run($rootScope) {
 function MainController(JsonService) {
     
     var vm = this;
-    vm.articles = {};
+    vm.articles = [];
     vm.heading = "Articles to Read";
+    vm.showForm = false;
+	vm.showResponse = false;
+	vm.formObject = {};
+
+	vm.addArticle = addArticle;
+	vm.deleteArticle = deleteArticle;
+	vm.editArticle = editArticle;
+	vm.exit = exit;
+	vm.saveArticle = saveArticle;
 
     JsonService.fetchArticles().then(fetchSuccess, fetchError);
 
     // End of calling code
+
+	function addArticle() {
+		console.log("Adding new article...");
+		vm.showForm = true;
+		vm.formObject.id = Date.now();
+		vm.formObject.position = vm.articles.length;
+
+	}
+
+	function deleteArticle(id, title) {
+		if(confirm("Are you sure you want to delete the entry: '" + title  + "' ?")) {
+			vm.articles = vm.articles.filter(function(article){ return article.id != id; }); // loose comparison for string-to-number comparison
+		}
+	}
+
+    function editArticle(id, title, link, position) {
+		vm.showForm = true;
+		vm.formObject.id = id;
+        vm.formObject.title = title;
+        vm.formObject.link = link;
+		vm.formObject.position = position;
+    }
+
+	function exit() {
+		vm.showResponse = true;
+	}
 
     function fetchError(response) {
         console.log(response);
@@ -25,6 +60,18 @@ function MainController(JsonService) {
     function fetchSuccess(response) {
         console.log(response.data);
         vm.articles = response.data;
+    }
+    
+    function saveArticle(formObject) {
+		console.log("Saving article...");
+        vm.showForm = false;
+        var article = {
+            id: formObject.id,
+            title: formObject.title,
+            link: formObject.link
+        };
+
+		vm.articles[formObject.position] = article;
     }
 }
 
